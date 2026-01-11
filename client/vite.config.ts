db80ee096d34e6f9df1150e08c2e16cc5198343e
Lomkiced <xanthosis122@gmail.com>
2026-01-11 14:45:34 +0800

    sms

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,12 @@
-import react from "@vitejs/plugin-react"
-import path from "path"
-import { defineConfig } from "vite"
+// FILE: client/vite.config.ts
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+
+// This is the fix for "type": "module"
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +15,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
